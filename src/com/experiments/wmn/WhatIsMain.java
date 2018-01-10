8de4c7c3076a524b16c72eb890fb8cd768eeb367
gabderakhmanov <gabderakhmanov@gmail.com>
2018-01-10 18:24:50 +0300

    psvm (What args contains)

---
+++ src/com/experiments/wmn/WhatIsMain.java
@@ -0,0 +1,14 @@
+package com.experiments.wmn;
+
+public class WhatIsMain {
+
+    public static void main(String[] args) {
+
+        System.out.println(args.length);
+
+        for (String arg : args){
+            System.out.println(arg);
+        }
+
+    }
+}
