49b19cc2ed62dfb6dd6493cefb5452fbfbc90487
gabderakhmanov <ugla87@yandex.ru>
2019-04-15 12:23:00 +0300

    CharToInt experiment

---
+++ src/com/experiments/chartoint/CharToInt.java
@@ -0,0 +1,13 @@
+package com.experiments.chartoint;
+
+public class CharToInt {
+
+    public static void main(String[] args) {
+
+        char myChar = 'J';
+
+        int intOfChar = myChar;
+
+        System.out.println("the int of char " +myChar+ " is " + intOfChar);
+    }
+}
