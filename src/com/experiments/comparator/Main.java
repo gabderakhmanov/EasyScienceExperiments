d928f288189ebf7e11bfaa43762eee90fa61fcce
gabderakhmanov <ugla87@yandex.ru>
2019-05-08 20:31:58 +0300

    add lambda and anonym

--- src/com/experiments/comparator/Main.java
+++ src/com/experiments/comparator/Main.java
@@ -1,6 +1,7 @@
 package com.experiments.comparator;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
 
@@ -8,7 +9,7 @@ public class Main {
 
         Animal anml1 = new Animal("1Dog", 165, 650);
         Animal anml2 = new Animal("Cat", 125, 750);
-        Animal anml3 = new Animal("Bird", 35, 350);
+        Animal anml3 = new Animal("Bird", 35, 1350);
 
         Animal[] animals = {anml1, anml2, anml3};
 
@@ -34,6 +35,22 @@ public class Main {
             System.out.println(animal);
         }
 
+        System.out.println("ComparatorBySpeed Anonymous");
+        Arrays.sort(animals, new Comparator<Animal>() {
+            @Override
+            public int compare(Animal o1, Animal o2) {
+                return o1.speed - o2.speed;
+            }
+        });
 
+        for (Animal animal : animals) {
+            System.out.println(animal);
+        }
+
+        System.out.println("ComparatorByPrice Anonymous lambda");
+        Arrays.sort(animals, (o1, o2) -> o1.price - o2.price);
+        for (Animal animal : animals) {
+            System.out.println(animal);
+        }
     }
 }
